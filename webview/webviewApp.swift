049687427cb77f7c1627e71778f4877ef2f31412
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-06-12 15:03:56 +0900

    Initial Commit

---
+++ webview/webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  webviewApp.swift
+//  webview
+//
+//  Created by 萩倉丈 on 2021/06/12.
+//
+
+import SwiftUI
+
+@main
+struct webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
